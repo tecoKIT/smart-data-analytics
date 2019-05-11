f8f6362c627ece47856cf4f7d4932476fb7d52da
ukhis <ukhis@student.kit.edu>
2019-05-11 14:44:07 +0200

    Comment poisson + beta regression files

--- Uebung1/module/beta_regression.py
+++ Uebung1/module/beta_regression.py
@@ -1,20 +1,37 @@
-import statsmodels.api as sm
-from sklearn.base import BaseEstimator, RegressorMixin
+# import libraries
+import statsmodels.api as sm  # for beta regression realization
+from sklearn.base import BaseEstimator, RegressorMixin  # for adapting a scikitlearn regressor
 
-
-# https://www.ime.usp.br/~sferrari/beta.pdf
-# https://www.statsmodels.org/stable/glm.html
-# https://gist.github.com/brentp/089c7d6d69d78d26437f
 
 class BetaRegression(BaseEstimator, RegressorMixin):
-    def __init__(self):
-        self.p = 1.0
-        self.q = 1.0
+    """ Lightweight implementation of a beta regressor using statsmodels library for the implementation
+        adapting the scikitlearn regressor base class to be able to use cross validation.
+        
+        Attributes:
+        gamma_model (statsmodels.genmod.generalized_linear_model.GLM): Model of the Beta Regression.
+        gamma_params (array): Parameters specifying the gamma model
+    """
 
-    def fit(self, X, y):
-        self.gamma_model = sm.GLM(y, X, family=sm.families.Gamma(link=sm.families.links.log))
+    def __init__(self):
+        """Initialization of the Beta Regression.
+        """
+        self.gamma_model = None
+        self.gamma_params = None
+
+    def fit(self, x, y):
+        """Fits the model using maximum likelihood.
+        Args:
+            x (array-like): A nobs x k array where nobs is the number of observations and k is the number of regressors.
+            y (array-like): 1-d endogenous response variable. The dependent variable.
+        """
+        self.gamma_model = sm.GLM(y, x, family=sm.families.Gamma(link=sm.families.links.log))
         self.gamma_params = self.gamma_model.fit().params
         return self
 
-    def predict(self, X):
-        return self.gamma_model.predict(self.gamma_params, X)
+    def predict(self, x_test):
+        """Predicts y for the given exogenous data.
+        Args:
+            x_test (array-like): A nobs x k array where nobs is the number
+            of observations and k is the number of regressors.
+        """
+        return self.gamma_model.predict(self.gamma_params, x_test)
--- Uebung1/module/poisson_regression.py
+++ Uebung1/module/poisson_regression.py
@@ -1,14 +1,50 @@
-import statsmodels.api as sm  # for poisson regression
-from sklearn.base import BaseEstimator, RegressorMixin
+# import libraries
+import statsmodels.api as sm  # for poisson regression realization
+from sklearn.base import BaseEstimator, RegressorMixin  # for adapting a scikitlearn regressor
+
 
 class PoissonRegression(BaseEstimator, RegressorMixin):
-    def __init__(self):
-        self.poisson_mod_ = None
-        self.poisson_res_ = None
+    """ Lightweight implementation of a poisson regressor using statsmodels library for the implementation
+        adapting the scikitlearn regressor base class to be able to use cross validation.
+        
+        Attributes:
+        poisson_mod (statsmodels.discrete.discrete_model.Poisson): Model of the Poisson Regression.
+        poisson_res (statsmodels.regression.linear_model.RegressionResults): Result of the Poisson Regression.
+        method (str, optional): The method determines which solver from scipy.optimize is used.
+    """
+
+    def __init__(self, method="newton"):
+        """Initialization of the Poisson Regression.
+
+        Args:
+            method (str): The method determines which solver from scipy.optimize is used,
+                and it can be chosen from among the following strings:
+                "newton" for Newton-Raphson, "nm" for Nelder-Mead
+                "bfgs" for Broyden-Fletcher-Goldfarb-Shanno (BFGS)
+                "lbfgs" for limited-memory BFGS with optional box constraints
+                "powell" for modified Powell"s method
+                "cg" for conjugate gradient
+                "ncg" for Newton-conjugate gradient
+                "basinhopping" for global basin-hopping solver
+                "minimize" for generic wrapper of scipy minimize (BFGS by default)
+        """
+        self.poisson_mod = None  # init instance attributes in constructor
+        self.poisson_res = None
+        self.method = method
 
     def fit(self, X, y):
-        self.poisson_mod_ = sm.Poisson(y, X)
-        self.poisson_res_ = self.poisson_mod_.fit(method="newton")
+        """Fits the model using maximum likelihood.
+        Args:
+            X (array-like): A nobs x k array where nobs is the number of observations and k is the number of regressors.
+            y (array-like): 1-d endogenous response variable. The dependent variable.
+        """
+        self.poisson_mod = sm.Poisson(y, X)
+        self.poisson_res = self.poisson_mod.fit(method=self.method)
         return self
+
     def predict(self, X_test):
-        return self.poisson_res_.predict(X_test)
+        """Predicts y for the given exogenous data.
+        Args:
+            X_test (array-like): A nobs x k array where nobs is the number of observations and k is the number of regressors.
+        """
+        return self.poisson_res.predict(X_test)
