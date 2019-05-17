1bef3b8699b054f7ef337f6b3e6b8020500eb7a1
Domi W <uiuqb@student.kit.edu>
2019-05-17 15:46:26 +0000

    Begin of exercise 5

---
+++ Uebung2/Exercise5/script_exercise5.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#setup anaconda
+. /etc/profile.d/anaconda.sh
+setup-anaconda
+# activate conda env
+source activate aufgabe1
+
+# run python
+python /smartdata/uiuqb/smart-data-analytics/Uebung2/Exercise5/Exercise3.py
